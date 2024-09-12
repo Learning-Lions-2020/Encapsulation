b2d53dc18655f26e30d7f7eb96ccf0f2a236db4e
Jerry Lokoroi <jerrylokoroi@gmail.com>
2024-09-12 10:11:04 +0300

    Add project files.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,67 @@
+﻿namespace EncapsulationDemo;
+
+public class BankAccount
+{
+    //private field - not accessible outside theclass
+    private decimal _balance;
+
+    public string AccountHolder { get; private set; }
+
+    public decimal Balance
+    {
+        get { return _balance; }
+        private set {   
+            if (value < 0 )
+            {
+                throw new ArgumentException($"Initial balance cannot be negative. Your current balance is: {Balance}");
+            }
+
+            _balance = value; }
+    }
+
+    /// <summary>
+    /// Constructor for the BankAccount class
+    /// </summary>
+    /// <param name="accountHolder"></param>
+    /// <param name="initialBalance"></param>
+    public BankAccount(string accountHolder, decimal initialBalance)
+    {
+        
+        AccountHolder = accountHolder;
+        Balance = initialBalance;
+    }
+
+    /// <summary>
+    /// Method for Deposit function
+    /// </summary>
+    /// <param name="amount"></param>
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+        }
+        Console.WriteLine($"Succesfully deposited {amount}");
+    }
+
+    /// <summary>
+    /// Method for Withdwal function
+    /// </summary>
+    /// <param name="amount"></param>
+    public void Withdraw(decimal amount)
+    {
+        if (amount > 0 && amount <= Balance)
+        {
+            Balance -= amount;
+        }
+        Console.WriteLine($"Succesfully withdrawn {amount}");
+
+    }
+
+    protected void ResetBalance()
+    {
+        _balance = 0;
+    }
+
+}
+
---
+++ BankAccount/Program.cs
@@ -0,0 +1,34 @@
+﻿using EncapsulationDemo;
+
+try
+{
+    var account = new BankAccount("Jerry Lokoroi", -10000);
+    Console.WriteLine($"Account Holder: {account.AccountHolder}");
+    Console.WriteLine($"Initial Balance: {account.Balance}");
+}
+catch (ArgumentException ex)
+{
+
+	Console.WriteLine(ex.Message );
+
+}
+
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
+
+
+
+
+
+/*account.Deposit(5000);
+Console.WriteLine($"New Account Balance: {account.Balance}");
+
+account.Withdraw(10000);
+Console.WriteLine($"New Account Balance: {account.Balance}");*/
+
+/*account.Balance = 100000;
+account.AccountHolder = "Bonny";*/
+
